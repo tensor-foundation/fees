22c5d66b3de0ec0d48129eb488f463ea5df36b2c
Samuel Vanderwaal <samv@tensor.so>
2024-11-16 13:24:29 -0900

    add t22 test

--- clients/js/test/collectTokens.test.ts
+++ clients/js/test/collectTokens.test.ts
@@ -17,6 +17,7 @@ import {
   createMint,
   generateKeyPairSignerWithSol,
   signAndSendTransaction,
+  TOKEN22_PROGRAM_ID,
   TOKEN_PROGRAM_ID,
 } from '@tensor-foundation/test-helpers';
 import test from 'ava';
@@ -31,7 +32,7 @@ const ONE_SOL = 10n ** 9n;
 const KEEP_ALIVE_LAMPORTS = 890880n;
 const AMOUNT = 10_000_000n;
 
-test('it can collect fees from sharded fee token accounts', async (t) => {
+test('it can collect fees from sharded fee legacy token accounts', async (t) => {
   const client = createDefaultSolanaClient();
   const payer = await generateKeyPairSignerWithSol(client);
   const treasury = address('243cQR2jrZQHQjReVpmq3DjPbqerJZEw7texPYudrfko');
@@ -113,6 +114,7 @@ test('it can collect fees from sharded fee token accounts', async (t) => {
         token,
         mintAuthority,
         amount: AMOUNT,
+        tokenProgram: TOKEN_PROGRAM_ID,
       });
     })
   );
@@ -127,6 +129,141 @@ test('it can collect fees from sharded fee token accounts', async (t) => {
     treasuryTa: treasuryAta,
     associatedTokenProgram: ASSOCIATED_TOKEN_ACCOUNTS_PROGRAM_ID,
     tokenProgram: TOKEN_PROGRAM_ID,
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, payer),
+    (tx) => appendTransactionMessageInstruction(collectTokensIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  const treasuryAtaBalance = (await fetchToken(client.rpc, treasuryAta)).data
+    .amount;
+
+  for (const token of tokens) {
+    const balance = (await fetchToken(client.rpc, token)).data.amount;
+    // Empty balance
+    t.assert(balance === 0n, 'Vault ATA balance is incorrect');
+  }
+
+  // Check that the treasury balance is the correct amount higher than the start balance.
+  t.assert(
+    treasuryAtaBalance ===
+      treasuryAtaStartBalance + AMOUNT * BigInt(tokens.length),
+    'Treasury balance is incorrect'
+  );
+});
+
+test('it can collect fees from sharded fee Token 2022 token accounts', async (t) => {
+  const client = createDefaultSolanaClient();
+  const payer = await generateKeyPairSignerWithSol(client);
+  const treasury = address('243cQR2jrZQHQjReVpmq3DjPbqerJZEw7texPYudrfko');
+  const mintAuthority = await generateKeyPairSignerWithSol(client);
+
+  // SPL Token mint
+  const mint = await createMint({
+    client,
+    payer,
+    decimals: 6,
+    mintAuthority: mintAuthority.address,
+    freezeAuthority: mintAuthority.address,
+    tokenProgram: TOKEN22_PROGRAM_ID,
+  });
+
+  const treasuryAta = await createAta({
+    client,
+    payer,
+    mint,
+    owner: treasury,
+    tokenProgram: TOKEN22_PROGRAM_ID,
+  });
+
+  const treasuryAtaStartBalance = (await fetchToken(client.rpc, treasuryAta))
+    .data.amount;
+
+  const numFeeAccounts = 5;
+
+  // Arbitrary keypairs to use to get the sharded fee accounts.
+  // In AMM, for example, these would be actual pool accounts.
+  const mints = await Promise.all(
+    Array.from(
+      { length: numFeeAccounts },
+      async () => await generateKeyPairSigner()
+    )
+  );
+
+  // Remove duplicates before we derive the PDAs.
+  const shardNumbers = Array.from(
+    new Set(mints.map((mint) => getShardNumber(mint.address)))
+  );
+
+  // Derive the fee accounts.
+  const pdas = await Promise.all(
+    shardNumbers.map(
+      async (shard) =>
+        await findFeeVaultPda({ shard: Uint8Array.from([shard]) })
+    )
+  );
+
+  // Build the bump, shard pairs for the collect instruction.
+  const seeds: FeeSeeds[] = pdas.map((pda, i) => ({
+    bump: pda[1],
+    shard: shardNumbers[i],
+  }));
+
+  // Fund the accounts
+  await Promise.all(
+    pdas.map(async (pda) => {
+      const vaultBalance = (await client.rpc.getBalance(pda[0]).send()).value;
+
+      return await airdropFactory(client)({
+        recipientAddress: pda[0],
+        lamports: lamports(ONE_SOL + KEEP_ALIVE_LAMPORTS - vaultBalance),
+        commitment: 'confirmed',
+      });
+    })
+  );
+
+  const vaults = pdas.map((pda) => pda[0]);
+
+  // Create the associated token accounts.
+  const tokens = await Promise.all(
+    vaults.map(async (vault) => {
+      return await createAta({
+        client,
+        payer,
+        mint,
+        owner: vault,
+        tokenProgram: TOKEN22_PROGRAM_ID,
+      });
+    })
+  );
+
+  // Mint to the associated token accounts.
+  await Promise.all(
+    tokens.map(async (token) => {
+      await mintTo({
+        client,
+        payer,
+        mint,
+        token,
+        mintAuthority,
+        amount: AMOUNT,
+        tokenProgram: TOKEN22_PROGRAM_ID,
+      });
+    })
+  );
+
+  const collectTokensIx = getCollectTokensInstruction({
+    treasury,
+    seeds,
+    vaults,
+    tokenAccounts: tokens,
+    payer,
+    mint,
+    treasuryTa: treasuryAta,
+    associatedTokenProgram: ASSOCIATED_TOKEN_ACCOUNTS_PROGRAM_ID,
+    tokenProgram: TOKEN22_PROGRAM_ID,
   });
 
   await pipe(
